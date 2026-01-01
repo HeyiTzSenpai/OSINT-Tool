702bd23f0e51149b834b55c0ec06a332f15d04f6
Walid <walidoo_2002@hotmail.com>
2025-12-31 19:16:50 -0500

    chore: set up initial command line argument for OSINT search tool

---
+++ osint_tool/main.py
@@ -0,0 +1,12 @@
+import argparse
+
+
+def main():
+    parser = argparse.ArgumentParser(description='OSINT Tool to search global information about a specific individual')
+    parser.add_argument('-p', '--prompt',
+                        help='Initial prompt to search using OSINT (e.g. First name, Last name, City)')
+    args = parser.parse_args()
+
+
+if __name__ == '__main__':
+    main()
